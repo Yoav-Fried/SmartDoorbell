7dbbbc3404ed309dc0e68712f9944c50ccac62d5
Yoav_Fried <47736688+Yoav-Fried@users.noreply.github.com>
2023-02-15 16:14:34 +0200

    Update Users.h

--- smart_doorbell/Users.h
+++ smart_doorbell/Users.h
@@ -1,12 +1,13 @@
 std::map<int, std::pair<std::string, std::string>> users;
+std::string love = "LOVE";
 
-std::string michal = "MICHAL";
-
+/*
+-- Enter Users with their matching links here, make sure links are https://
+*/
 void populate_users() {
-  users[12345] = std::make_pair("YOAV", "https://bit.ly/3S2j54t");
-  users[11111] = std::make_pair("TAL", "https://bit.ly/3Xp6qJz");
-  users[22222] = std::make_pair("MICHAL", "https://bit.ly/3HYPuEa");
-  users[33333] = std::make_pair("ZVIKA", "https://bit.ly/3lm9tF8");
+  users[12345] = std::make_pair("Me", "https://bit.ly/3YB7qfo");
+  users[22222] = std::make_pair("LOVE", "https://bit.ly/3lBiTNc");
+  users[33333] = std::make_pair("RICK", "https://bit.ly/3lm9tF8");
 }
 
 std::string get_name(int key) {
